9a455bb90a7dd56fc6663ecdf6a2106fb6a54519
Sam Minot <sminot@fredhutch.org>
2024-02-07 11:27:24 -0800

    Try getCellObjects

--- assets/StarDist_cell_segmentation.groovy
+++ assets/StarDist_cell_segmentation.groovy
@@ -75,7 +75,7 @@ def exporter  = new MeasurementExporter()
 // Export the cell shapes as GeoJSON
 println 'Exporting cell shapes'
 
-def annotations = imageData.getHierarchy().getAllObjects()
+def annotations = imageData.getHierarchy().getCellObjects()
 def geoJsonPath = args[4]
 exportObjectsToGeoJson(annotations, geoJsonPath)
 
