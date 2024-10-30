74e2a1fc53d0dabd03dd854a7a25dd4582721c7c
Sam Minot <sminot@gmail.com>
2024-10-30 16:17:29 -0700

    Configure outputs to look like xenium as well

--- templates/configure_vitessce.py
+++ templates/configure_vitessce.py
@@ -20,16 +20,11 @@ def format_vitessce_segmentation(
     description: str,
     zarr_fp: str,
     image_key: str,
-    obs_set_paths: List[str],
-    obs_set_names: List[str],
-    init_gene: str,
     channel_names: list,
     mask_channels: list,
     schema_version = "1.0.16",
     obs_type = "cell",
-    feature_type = "marker",
-    spots_key = "cells",
-    feature_value_type = "expression"
+    **kwargs
 ):
     
     # Set up the channels that will be displayed.
@@ -223,6 +218,442 @@ def format_vitessce_segmentation(
     }
 
 
+def format_vitessce_cell_measurements(
+    name: str,
+    description: str,
+    zarr_fp: str,
+    image_key: str,
+    obs_set_paths: List[str],
+    obs_set_names: List[str],
+    init_gene: str,
+    schema_version = "1.0.16",
+    obs_type = "cell",
+    feature_type = "marker",
+    spots_key = "centroids",
+    feature_value_type = "expression",
+    **kwargs
+):
+    return {
+            "version": schema_version,
+            "name": name,
+            "description": description,
+            "datasets": [
+                {
+                    "uid": "A",
+                    "name": name,
+                    "files": [
+                        {
+                            "url": zarr_fp,
+                            "fileType": "image.spatialdata.zarr",
+                            "coordinationValues": {
+                                "fileUid": image_key,
+                                "obsType": obs_type
+                            },
+                            "options": {
+                                "path": f'images/{image_key}'
+                            }
+                        },
+                        {
+                            "url": zarr_fp,
+                            "fileType": "obsFeatureMatrix.spatialdata.zarr",
+                            "coordinationValues": {
+                                "obsType": obs_type
+                            },
+                            "options": {
+                                "path": "tables/table/X"
+                            }
+                        },
+                        {
+                            "url": zarr_fp,
+                            "fileType": "obsSpots.spatialdata.zarr",
+                            "coordinationValues": {
+                                "obsType": obs_type
+                            },
+                            "options": {
+                                "path": f"shapes/{spots_key}",
+                                "tablePath": "tables/table"
+                            }
+                        },
+                        {
+                            "url": zarr_fp,
+                            "fileType": "obsSets.spatialdata.zarr",
+                            "coordinationValues": {
+                                "obsType": obs_type
+                            },
+                            "options": {
+                                "obsSets": [
+                                    {
+                                        "name": name,
+                                        "path": f"tables/table/{path}"
+                                    }
+                                    for path, name in zip(
+                                        obs_set_paths,
+                                        obs_set_names
+                                    )
+                                ]
+                            }
+                        }
+                    ]
+                }
+            ],
+            "coordinationSpace": {
+                "dataset": {
+                    "A": "A"
+                },
+                "featureSelection": {
+                    "A": [
+                        init_gene
+                    ],
+                    "B": [
+                        init_gene
+                    ]
+                },
+                "obsType": {
+                    "A": obs_type
+                },
+                "featureType": {
+                    "A": feature_type
+                },
+                "featureValueType": {
+                    "A": feature_value_type
+                },
+                "obsColorEncoding": {
+                    "A": "cellSetSelection",
+                    "B": "geneSelection"
+                },
+                "spatialTargetZ": {
+                    "A": 0
+                },
+                "spatialTargetT": {
+                    "A": 0
+                },
+                "imageLayer": {
+                    "A": "__dummy__",
+                    "B": "__dummy__"
+                },
+                "fileUid": {
+                    "A": image_key,
+                    "B": image_key
+                },
+                "spatialLayerOpacity": {
+                    "A": 1,
+                    "B": 0.5,
+                    "C": 1,
+                    "D": 0.5
+                },
+                "spatialLayerVisible": {
+                    "A": True,
+                    "B": True,
+                    "C": True,
+                    "D": True
+                },
+                "photometricInterpretation": {
+                    "A": "BlackIsZero",
+                    "B": "BlackIsZero"
+                },
+                "imageChannel": {
+                    "A": "__dummy__",
+                    "B": "__dummy__"
+                },
+                "spatialTargetC": {
+                    "A": 0,
+                    "B": 0,
+                },
+                "spatialChannelColor": {
+                    "A": [255, 255, 255],
+                    "B": [255, 255, 255]
+                },
+                "spatialChannelWindow": {
+                    "A": None,
+                    "B": None
+                },
+                "spatialChannelVisible": {
+                    "A": True,
+                    "B": True
+                },
+                "spatialChannelOpacity": {
+                    "A": 1,
+                    "B": 1
+                },
+                "spotLayer": {
+                    "A": "__dummy__",
+                    "B": "__dummy__"
+                },
+                "spatialSpotRadius": {
+                    "A": 20,
+                    "B": 20
+                },
+                "spatialLayerColormap": {
+                    "A": None,
+                    "B": None
+                },
+                "featureValueColormap": {
+                    "A": "plasma",
+                    "B": "plasma"
+                },
+                "featureValueColormapRange": {
+                    "A": [
+                        0,
+                        1.0
+                    ],
+                    "B": [
+                        0,
+                        1.0
+                    ]
+                },
+                "metaCoordinationScopes": {
+                    "A": {
+                        "spatialTargetZ": "A",
+                        "spatialTargetT": "A",
+                        "obsType": "A",
+                        "imageLayer": "A",
+                        "spotLayer": "A",
+                        "obsColorEncoding": "A",
+                        "featureSelection": "A"
+                    },
+                    "B": {
+                        "spatialTargetZ": "A",
+                        "spatialTargetT": "A",
+                        "obsType": "A",
+                        "imageLayer": "B",
+                        "spotLayer": "B",
+                        "obsColorEncoding": "B",
+                        "featureSelection": "B"
+                    }
+                },
+                "metaCoordinationScopesBy": {
+                    "A": {
+                        "imageLayer": {
+                            "fileUid": {
+                                "A": "A"
+                            },
+                            "spatialLayerOpacity": {
+                                "A": "A"
+                            },
+                            "spatialLayerVisible": {
+                                "A": "A"
+                            },
+                            "photometricInterpretation": {
+                                "A": "A"
+                            },
+                            "spatialLayerColormap": {
+                                "A": "A"
+                            },
+                            "imageChannel": {
+                                "A": [
+                                    "A"
+                                ]
+                            }
+                        },
+                        "spotLayer": {
+                            "spatialLayerOpacity": {
+                                "A": "B"
+                            },
+                            "spatialLayerVisible": {
+                                "A": "B"
+                            },
+                            "spatialLayerColor": {
+                                "A": "A"
+                            },
+                            "obsColorEncoding": {
+                                "A": "A"
+                            },
+                            "spatialSpotRadius": {
+                                "A": "A"
+                            }
+                        },
+                        "imageChannel": {
+                            "spatialTargetC": {
+                                "A": "A"
+                            },
+                            "spatialChannelColor": {
+                                "A": "A"
+                            },
+                            "spatialChannelWindow": {
+                                "A": "A"
+                            },
+                            "spatialChannelVisible": {
+                                "A": "A"
+                            },
+                            "spatialChannelOpacity": {
+                                "A": "A"
+                            }
+                        }
+                    },
+                    "B": {
+                        "imageLayer": {
+                            "fileUid": {
+                                "B": "B"
+                            },
+                            "spatialLayerOpacity": {
+                                "B": "C"
+                            },
+                            "spatialLayerVisible": {
+                                "B": "C"
+                            },
+                            "photometricInterpretation": {
+                                "B": "B"
+                            },
+                            "spatialLayerColormap": {
+                                "B": "B"
+                            },
+                            "imageChannel": {
+                                "B": [
+                                    "B"
+                                ]
+                            }
+                        },
+                        "spotLayer": {
+                            "spatialLayerOpacity": {
+                                "B": "D"
+                            },
+                            "spatialLayerVisible": {
+                                "B": "D"
+                            },
+                            "spatialLayerColor": {
+                                "B": "B"
+                            },
+                            "obsColorEncoding": {
+                                "B": "B"
+                            },
+                            "spatialSpotRadius": {
+                                "B": "B"
+                            }
+                        },
+                        "imageChannel": {
+                            "spatialTargetC": {
+                                "B": "B"
+                            },
+                            "spatialChannelColor": {
+                                "B": "B"
+                            },
+                            "spatialChannelWindow": {
+                                "B": "B"
+                            },
+                            "spatialChannelVisible": {
+                                "B": "B"
+                            },
+                            "spatialChannelOpacity": {
+                                "B": "B"
+                            }
+                        }
+                    }
+                }
+            },
+            "layout": [
+                {
+                    "component": "spatialBeta",
+                    "coordinationScopes": {
+                        "dataset": "A",
+                        "metaCoordinationScopes": [
+                            "A"
+                        ],
+                        "metaCoordinationScopesBy": [
+                            "A"
+                        ]
+                    },
+                    "x": 0,
+                    "y": 0,
+                    "w": 4,
+                    "h": 6
+                },
+                {
+                    "component": "layerControllerBeta",
+                    "coordinationScopes": {
+                        "dataset": "A",
+                        "metaCoordinationScopes": [
+                            "A"
+                        ],
+                        "metaCoordinationScopesBy": [
+                            "A"
+                        ]
+                    },
+                    "x": 0,
+                    "y": 6,
+                    "w": 4,
+                    "h": 3
+                },
+                {
+                    "component": "spatialBeta",
+                    "coordinationScopes": {
+                        "dataset": "A",
+                        "metaCoordinationScopes": [
+                            "B"
+                        ],
+                        "metaCoordinationScopesBy": [
+                            "B"
+                        ]
+                    },
+                    "x": 4,
+                    "y": 0,
+                    "w": 4,
+                    "h": 6
+                },
+                {
+                    "component": "layerControllerBeta",
+                    "coordinationScopes": {
+                        "dataset": "A",
+                        "metaCoordinationScopes": [
+                            "B"
+                        ],
+                        "metaCoordinationScopesBy": [
+                            "B"
+                        ]
+                    },
+                    "x": 4,
+                    "y": 6,
+                    "w": 4,
+                    "h": 3
+                },
+                {
+                    "component": "heatmap",
+                    "coordinationScopes": {
+                        "dataset": "A",
+                        "featureSelection": "B"
+                    },
+                    "x": 0,
+                    "y": 9,
+                    "w": 8,
+                    "h": 3
+                },
+                {
+                    "component": "obsSetSizes",
+                    "coordinationScopes": {
+                        "obsType": "A",
+                        "dataset": "A"
+                    },
+                    "x": 8,
+                    "y": 0,
+                    "w": 2,
+                    "h": 6
+                },
+                {
+                    "component": "featureList",
+                    "coordinationScopes": {
+                        "dataset": "A",
+                        "featureSelection": "B"
+                    },
+                    "x": 10,
+                    "y": 0,
+                    "w": 2,
+                    "h": 6
+                },
+                {
+                    "component": "obsSetFeatureValueDistribution",
+                    "coordinationScopes": {
+                        "dataset": "A",
+                        "featureSelection": "B"
+                    },
+                    "x": 8,
+                    "y": 6,
+                    "w": 4,
+                    "h": 6
+                }
+            ],
+            "initStrategy": "auto"
+        }
+
+
 def main():
     # Read in the spatialdata.kwargs.json file
     with open("spatialdata.kwargs.json", "r") as f:
@@ -231,7 +662,8 @@ def main():
     # Configure the viewer twice, once to show segmentation
     # and a second time to show cell measurements
     for prefix, vt_config in [
-        ("segmentation", format_vitessce_segmentation(**vt_kwargs))
+        ("segmentation", format_vitessce_segmentation(**vt_kwargs)),
+        ("cell_measurements", format_vitessce_cell_measurements(**vt_kwargs))
     ]:
         # Save the configuration to JSON
         with open(f"{prefix}.vt.json", "w") as f:
